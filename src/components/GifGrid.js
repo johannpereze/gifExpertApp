9747750b6c6caf08e4d6d896b9afb80d88d7ec42
johannpereze <sebas.jsp@gmail.com>
2021-11-11 09:00:45 -0500

    Ya cargan las imagenes y pasamos la funcion de fectch a un archivo independiente. ademas se agregaron algunos estilos

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,37 +1,24 @@
 import { useEffect, useState } from "react";
+import { getGifs } from "../helpers/getGifs";
+import { GifGridItem } from "./GifGridItem";
 require("dotenv").config();
 
 export const GifGrid = ({ category }) => {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    getGifs();
-  }, []);
-
-  const getGifs = async () => {
-    const REACT_APP_GIPHY_API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
-    const url = `https://api.giphy.com/v1/gifs/search?q=one punch&limit=10&api_key=${REACT_APP_GIPHY_API_KEY}`;
-    const resp = await fetch(url);
-    const { data } = await resp.json();
-    const gifs = data.map((img, title) => {
-      return {
-        id: img.id,
-        title: img.title,
-        url: img.images.downsized_medium.url,
-      };
-    });
-    console.log(gifs);
-    setImages(gifs);
-  };
+    getGifs(category)
+    .then(imgs=>setImages(imgs));
+  }, [category]);
 
   return (
-    <div>
+    <>
       <h3>{category}</h3>
-      <ol>
-        {images.map(({id, title}) => {
-          return <li key={id}>{title}</li>;
+      <div className={"card-grid"}>
+        {images.map((img) => {
+          return <GifGridItem key={img.id} {...img} />;
         })}
-      </ol>
-    </div>
+      </div>
+    </>
   );
 };
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+export const getGifs = async (category) => {
+  const REACT_APP_GIPHY_API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
+    category
+  )}&limit=10&api_key=${REACT_APP_GIPHY_API_KEY}`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+  const gifs = data.map((img, title) => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images.downsized_medium.url,
+    };
+  });
+  return gifs;
+};
