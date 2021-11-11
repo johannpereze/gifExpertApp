2bdced13792cd4e8bb513dea05ac5d7fac760f74
johannpereze <sebas.jsp@gmail.com>
2021-11-11 13:04:08 -0500

    Ponemnos animaciones a la App y queda listo el primer release

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -10,7 +10,7 @@ export const GifGrid = ({ category }) => {
 
   return (
     <>
-      <h3>{category}</h3>
+      <h3 className={"animate__animated animate__fadeIn"}>{category}</h3>
       {loading && <p>Cargando...</p>}
       <div className={"card-grid"}>
         {images.map((img) => {
