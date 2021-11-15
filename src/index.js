5954c494e5ddd1e822447a278a1d6f8ee2c7ef46
johannpereze <sebas.jsp@gmail.com>
2021-11-15 10:02:51 -0500

    prueba ssh

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {GifExpertApp} from "./GifExpertApp";
+import { configure } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+import { GifExpertApp } from "./GifExpertApp";
 
 import './index.css'
 
+configure({ adapter: new Adapter() });
+
 ReactDOM.render(<GifExpertApp />, document.getElementById("root"));
+
+
